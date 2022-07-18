085be947ca397301e63dc9843b5b98e6abda37c9
Lúcio Hermano Batista <prluciohermano@gmail.com>
2022-07-18 00:56:23 -0300

    Arrumando o Link 2

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,22 @@ import Link from 'next/link';
 
 function Header() {
     return(
+        <div>
+            <h1>Garage Control</h1>
+            <p>
+                Desenvolvido por 
+                <a href="https://www.instagram.com/prluciohermano"> @prluciohermano
+
+                </a>
+            </p>
             <div>
-                <h1>Garage Control</h1>
-                <p>
-                    Desenvolvido por 
-                    <a href="https://www.instagram.com/prluciohermano"> @prluciohermano
+                <h2>Sobre</h2>
 
-                    </a>
-                </p>
-                <div>
-                    <h2>Sobre</h2>
-
-                    <Link href="/sobre">
-                        <a>Acessar página sobre</a>
-                    </Link>
-                </div>
+                <Link href="/sobre">
+                    <a>Acessar página sobre</a>
+                </Link>
             </div>
+        </div>
         
     )
 }
--- pages/sobre.js
+++ pages/sobre.js
@@ -2,22 +2,22 @@ import Link from 'next/link';
 
 function Sobre() {
     return(
+        <div>
+            <h1>Garage Control</h1>
+            <p>
+                Desenvolvido por 
+                <a href="https://www.instagram.com/prluciohermano"> @prluciohermano
+
+                </a>
+            </p>
             <div>
-                <h1>Garage Control</h1>
-                <p>
-                    Desenvolvido por 
-                    <a href="https://www.instagram.com/prluciohermano"> @prluciohermano
+                <h2>Index</h2>
 
-                    </a>
-                </p>
-                <div>
-                    <h2>Index</h2>
-
-                    <Link href="/index">
-                        <a>Acessar página index </a>
-                    </Link>
-                </div>
+                <Link href="/">
+                    <a>Acessar página index </a>
+                </Link>
             </div>
+        </div>
         
     )
 }
