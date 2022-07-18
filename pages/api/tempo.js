df60766bc790b7add9e5bdcba184dd468095b93f
Lúcio Hermano Batista <prluciohermano@gmail.com>
2022-07-18 12:56:46 -0300

    Adiciona variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,15 @@
-function Tempo(request, response) {
+async function tempo(request, response) {
+    const api_secret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/account?api_secret=${api_secret}`);
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson.total_subscribers;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
     })
 }
 
-export default Tempo;
+export default tempo;
